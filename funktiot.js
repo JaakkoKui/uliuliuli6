a62aea6f9c43f026343e6f11adf5aa9483fbf18e
JaakkoKui <50982637+JaakkoKui@users.noreply.github.com>
2019-06-05 13:33:13 +0300

    Add files via upload

---
+++ funktiot.js
@@ -0,0 +1,10 @@
+
+function myfunction() {
+document.body.style.backgroundColor = "lightblue"}
+function myfunction2() {
+document.body.style.backgroundColor = "lightgreen"}
+function myfunction3() {
+document.getElementById('demo').innerHTML = Date()}
+function myfunction4() {
+document.getElementById('Moro').innerHTML =':)'}
+
